024cef0a1c1511a0220e38b261645a95d80d4bc5
Dandi Ahmadin <ahmadin.dandi14@gmail.com>
2020-10-16 09:57:59 +0700

    Initial Commit

---
+++ src/if1/pkg10119033/latihan2/deklarasivariabel/IF110119033Latihan2DeklarasiVariabel.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119033.latihan2.deklarasivariabel;
+
+
+/**
+ *
+ * @author
+ * NAMA     : Dandi Ahmadin
+ * KELAS    : IF-1
+ * NIM      : 10119033
+ * Deskripsi Program : Program ini berisi deklarasi dan pengaksesan variable
+ */
+public class IF110119033Latihan2DeklarasiVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Deklarasi Variabel
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        // Memberi nilai kevariabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+                
+        // Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel logika = " + nilaiLogika);
+        System.out.println("Isi variabel karakter = " + nilaiKarakter);
+    }
+    
+}
